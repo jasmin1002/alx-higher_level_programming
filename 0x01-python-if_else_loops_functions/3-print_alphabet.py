99be5074b2e2fd0b454943a1518fde8e50c3da02
Jasmine <jasmineolofin@gmail.com>
2022-05-31 16:43:57 +0000

    file deleted

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-asciiNumber = 97
-while asciiNumber <= 122:
-    if asciiNumber in [101, 113]:
-        asciiNumber += 1
-        continue
-
-    print("{}".format(chr(asciiNumber)), end="")
-    asciiNumber += 1
