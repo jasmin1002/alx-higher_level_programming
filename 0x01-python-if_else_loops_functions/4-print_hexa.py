600bf3ff97076498da21c81bab5f91a29c3ac363
Jasmine <jasmineolofin@gmail.com>
2022-06-01 21:53:46 +0000

    string format-3 added

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    print(f"{number:d} = {hex(number)}")
+    print("{} = {}".format(number, hex(number)))
+#print("{0:d} = {1:s}".format(number, hex(number)))
