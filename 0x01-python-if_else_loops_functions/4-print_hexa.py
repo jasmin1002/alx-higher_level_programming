dc3a5a5d33b39aa769c778efc21fa24807b9ec3d
Jasmine <jasmineolofin@gmail.com>
2022-06-01 21:56:09 +0000

    positional argument adjusted in string format

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
-#print("{0:d} = {1:s}".format(number, hex(number)))
+    print("{0:d} = {1:s}".format(number, hex(number)))
