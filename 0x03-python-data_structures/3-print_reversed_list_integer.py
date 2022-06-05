d53ead5b38606b1f77a576c84da491f9dd3a5154
Jasmine <jasmineolofin@gmail.com>
2022-06-05 08:41:35 +0000

    added condition to confirm passed argument to be list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        return
-    my_list.reverse()
-    for digit in my_list:
-        print("{:d}".format(digit))
+    if type(my_list) is list:
+        my_list.reverse()
+        for digit in my_list:
+            print("{:d}".format(digit))
