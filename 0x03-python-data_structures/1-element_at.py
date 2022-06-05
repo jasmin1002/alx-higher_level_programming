4650006e22aaa6b73cd3e5d1cbd7a199800b50f6
Jasmine <jasmineolofin@gmail.com>
2022-06-05 06:39:12 +0000

    out of range for list fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list=[], idx=0):
-    value = None if idx < 0 or idx > len(my_list) else my_list[idx]
+    value = None if idx < 0 or idx >= len(my_list) else my_list[idx]
     return value
