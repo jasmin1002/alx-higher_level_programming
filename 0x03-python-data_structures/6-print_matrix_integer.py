c0a3693c3a7d70bae17fcc5189b21acfcee6dc42
Jasmine <jasmineolofin@gmail.com>
2022-06-06 11:13:34 +0000

    Added confirmation for being a list and removed trailing whitespace

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if type(matrix) is not list:
+        return
     if len(matrix[0]) == 0:
         print('')
     else:
@@ -8,6 +10,6 @@ def print_matrix_integer(matrix=[[]]):
             while idx < len(matrix[i]):
                 if idx == len(matrix[i]) - 1:
                     print("{:d}".format(matrix[i][idx]))
-                    break 
+                    break
                 print("{:d}".format(matrix[i][idx]), end=' ')
                 idx += 1
