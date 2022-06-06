dafdce9a2819f9922c985eaf8cc1647ce9b38de7
Jasmine <jasmineolofin@gmail.com>
2022-06-06 11:03:11 +0000

    6-print_matrix_integer.py prints any kind of matrix with their fixed format structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print('')
+    else:
+        for i in range(len(matrix)):
+            idx = 0
+            while idx < len(matrix[i]):
+                if idx == len(matrix[i]) - 1:
+                    print("{}".format(matrix[i][idx]))
+                    break 
+                print("{}".format(matrix[i][idx]), end=' ')
+                idx += 1
