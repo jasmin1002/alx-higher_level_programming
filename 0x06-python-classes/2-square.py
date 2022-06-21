1567318b8bb7783acc0896360e17b856b933d20f
Jasmine <jasmineolofin@gmail.com>
2022-06-21 17:08:12 +0000

    Replace try-except with raise

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,11 +16,9 @@ class Square:
 
     """
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError
-            self.__size = int(size)
-        except ValueError:
-            print("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
