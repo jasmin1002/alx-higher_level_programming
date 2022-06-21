5533a1f7b617daa4e9f01ce8f6362447db846730
Jasmine <jasmineolofin@gmail.com>
2022-06-21 15:59:19 +0000

    Square class commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Square defines a square plane object by size
+
+    The __init__ sets default value for size
+
+    Args:
+        size(int): describes size of a square
+
+    Attributes:
+        size(int): describes size of a square
+
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
