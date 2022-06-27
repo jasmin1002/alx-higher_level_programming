23a147315cfb89c792c808f56b418e260557a93d
Jasmine <jasmineolofin@gmail.com>
2022-06-27 16:29:25 +0000

    adjust the private instance variable

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle():
         height: zero or positive int
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
