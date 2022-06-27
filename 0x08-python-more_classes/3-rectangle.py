e6999019981da79c6e28a42dc95b7dd4e72dc0ab
Jasmine <jasmineolofin@gmail.com>
2022-06-27 18:51:54 +0000

    Add the else clause

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle():
         height: int
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -33,7 +33,8 @@ class Rectangle():
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -47,7 +48,8 @@ class Rectangle():
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         """Computes the area of rectangle instance"""
