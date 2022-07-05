66b779a09f8bb49c22c006bd4fdfb09c393b5de2
Jasmine <jasmineolofin@gmail.com>
2022-07-05 01:21:50 +0000

    commit my_list module

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Class Mylist that inherits
+    from list
+"""
+
+
+class MyList(list):
+    """Define Myclass subclass
+    Args:
+        list (list): class list
+    """
+    def print_sorted(self):
+        """Prints the list sorted
+        """
+        print(sorted(self))
