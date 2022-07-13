bd0cf9f7be656b650c74beafddac084fc2cfa877
Jasmine <jasmineolofin@gmail.com>
2022-07-13 02:29:23 +0000

    add update method to instance methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,3 +128,14 @@ class Rectangle(Base):
                 self.width,
                 self.height
         )
+
+    def update(self, *args):
+        """Update modifies class instance's value"""
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
