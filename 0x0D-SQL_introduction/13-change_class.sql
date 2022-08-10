59cd28cb722578eb0ae0bdf3c0ed5e2388cf2244
Jasmine <jasmineolofin@gmail.com>
2022-08-09 22:13:27 -0700

    Modified the command to delete a record

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- This script removes all records with a point
 -- below 5 or equl 5 in the table second_table of 
 -- database hbtn_0c_0 in MySQL.
-DELETE * FROM second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
