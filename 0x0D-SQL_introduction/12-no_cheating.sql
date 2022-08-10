973bf2d7d25a5b7cac959bae458e6d0202df90c9
Jasmine <jasmineolofin@gmail.com>
2022-08-09 21:56:37 -0700

    Added a script that update a record

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- This script updates the score of a student
+-- name Bob to 10 in the table second_table of
+-- database hbtn_0c_c in MySQL server.
+UPDATE second_table SET score = 10
+	WHERE name = 'Bob';
