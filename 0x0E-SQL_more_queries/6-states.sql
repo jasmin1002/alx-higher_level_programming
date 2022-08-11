3d69d3e1879fce0150ae8c2dda4136858b2eca76
Jasmine <jasmineolofin@gmail.com>
2022-08-11 11:36:37 -0700

    Used database.tablename syntax

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- a table states (in the databse hbtn_0d_usa) on
 -- MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL UNIQUE,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id)
