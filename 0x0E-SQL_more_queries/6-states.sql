8eb781f48f8e2d5e5799f78594b469233f97564a
Jasmine <jasmineolofin@gmail.com>
2022-08-10 22:06:01 -0700

    Added a script that creates a table with unique and not null fields

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- This script creates a database hbtn_0d_usa and
+-- a table states (in the databse hbtn_0d_usa) on
+-- MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
+
