b2056a59f367618e7ed5357d599593bb6e1a24e6
Jasmine <jasmineolofin@gmail.com>
2022-08-11 20:32:11 -0700

    Added a script to create database and table using FOREIGN KEY

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- This script creates a database hbtn_0d_usa and
+-- a table cities (in the database hbtn_0d_usa) on
+-- MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	PRIMARY KEY(id),
+	CONSTRAINT FOREIGN KEY(state_id) REFERENCES states(id)
+);
