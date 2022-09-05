0ae2a77e3b713a2ac5d2981943653d2c1e756f44
Jasmine <jasmineolofin@gmail.com>
2022-09-05 10:06:56 -0700

    Optimized code: striped off redundancy lines of codes

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,9 +6,5 @@ function add (a, b) {
   return a + b;
 }
 
-if (!inputs.length) {
-  console.log(Number(inputs[0]));
-} else {
-  const sum = add(Number(inputs[0]), Number(inputs[1]));
-  console.log(sum);
-}
+const sum = add(Number(inputs[0]), Number(inputs[1]));
+console.log(sum);
