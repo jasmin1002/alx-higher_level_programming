5aeccf3eade94d6aae7a2dd88ae906c6b4634aef
Jasmine <jasmineolofin@gmail.com>
2022-09-06 08:41:58 -0700

    Added a check for undefined value

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,6 +2,8 @@
 exports.nbOccurrences = function (list, searchElement) {
   let times = 0; // keep track of number of occurrence
 
+  if (!searchElement || !list[0]) return;
+
   // Iterate over given list.
   list.forEach(item => {
     if (item === searchElement) times += 1;
