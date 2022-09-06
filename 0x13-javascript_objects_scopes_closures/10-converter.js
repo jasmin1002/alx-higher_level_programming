b6e0a15c60281fe9de73108bbedbd7519052e4bc
Jasmine <jasmineolofin@gmail.com>
2022-09-06 11:08:56 -0700

    Added a script for conversion

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.converter = (function () {
+  return function (base) {
+    return function (number) {
+      const result = number.toString(base);
+
+      return result;
+    };
+  };
+}());
