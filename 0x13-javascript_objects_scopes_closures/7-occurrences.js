4c60bb4147e02e66f3140a11b91313d63d15b85a
Jasmine <jasmineolofin@gmail.com>
2022-09-06 08:57:23 -0700

    Added back the array forEach method

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let times = 0; // keep track of number of occurrence
 
   if (!searchElement || !list[0]) return -1;
 
   // Iterate over given list.
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) times++;
-  }
+  list.forEach(item => {
+    if (item === searchElement) times++;
+  });
 
   // Return No. of occurrence.
   return times;
