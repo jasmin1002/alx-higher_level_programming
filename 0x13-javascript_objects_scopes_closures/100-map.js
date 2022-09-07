81f81fea013df5be6326f3775fdf960f7a6921b3
Jasmine <jasmineolofin@gmail.com>
2022-09-06 19:18:31 -0700

    Removed template literal and Added comments

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+// Import data file
 const list = require('./100-data').list;
+
+// Transform each element of input array, list
+// by multiplying each element with its index.
 const nList = list.map((item, idx) => idx * item);
-console.log(`${list}\n${nList}`);
+
+// Print both arrays.
+console.log(list);
+console.log(nList);
