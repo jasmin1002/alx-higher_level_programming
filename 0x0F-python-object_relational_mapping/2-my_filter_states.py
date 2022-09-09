e348d91fce084fd1786c27c0afd76469db7445e5
Jasmine <jasmineolofin@gmail.com>
2022-09-09 06:47:09 -0700

    Added a script filter query by user's input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+'''
+    This script connects to MySQL database using
+    MySQLdb module.
+
+    It returns a queryset of state_name filter by
+    user's input of name of a state after successful
+    connection.
+'''
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    inputs = sys.argv[1:]
+
+    # Unpack input values
+    try:
+        print(inputs)
+        mysql_username, mysql_passwd, db_name, s_name = inputs
+    except ValueError:
+        msg = "Not enough inputs (expect 3, got {})".format(len(inputs))
+        print(msg)
+
+        # Exit the program if No. of inputs < 3
+        sys.exit(1)
+
+    try:
+        # Create connection to MySQL database
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_passwd,
+            db=db_name
+        )
+
+        # Create a cursor to execute a query
+        cur = db.cursor()
+
+        # query
+        query = "SELECT * FROM states WHERE name = '{}'\
+                ORDER BY id".format(s_name)
+        cur.execute(query)
+
+        # Retrieve and store the queryset in state variable
+        state = cur.fetchone()
+
+        # Print the result to console (stdout)
+        print(state)
+
+        # Close open connection
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as err:
+        print("MySQL Error: {}".format(err.args[1]))
