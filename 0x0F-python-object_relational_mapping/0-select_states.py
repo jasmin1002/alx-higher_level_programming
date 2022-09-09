55d5590a9deec4a71328ad650c33ebeee756090a
Jasmine <jasmineolofin@gmail.com>
2022-09-09 03:50:37 -0700

    Added a script, connect to MySQL database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    inputs = sys.argv[1:]
+    try:
+        mysql_username, mysql_passwd, db_name = inputs
+    except ValueError:
+        print(
+            "Not enough inputs (expected 3, got {})".format(len(inputs))
+        )
+        sys.exit(1)
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_passwd,
+            db=db_name
+        )
+
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+        states = cur.fetchall()
+
+        for state in states:
+            print(state)
+
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e.args[1]))
