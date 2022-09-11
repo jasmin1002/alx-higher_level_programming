cfef9bd311ba9cb65be1421f0a05fe4b5de50213
Jasmine <jasmineolofin@gmail.com>
2022-09-10 17:04:56 -0700

    Added a city model class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+    City Model class description
+'''
+
+from sqlachemy import (Column, Integer, String, ForeignKey)
+from model_state import Base
+
+
+class City(Base):
+    '''
+    A row representation for city entry in cities table
+
+
+    Args:
+        name (str): city name
+        state_id (int): reference id to parent table (states)
+
+
+    Attributes:
+        id (int): auto-populate data column
+        name (str): Descriptive name data column
+        state_id (int): Normalised foreign key column
+    '''
+
+    #: list of tuple: cities table
+    __tablename__ = 'cities'
+
+    id = Column(
+        Integer,
+        nullable=False,
+        primary_key=True,
+        autoincrement='auto',
+        unique=True
+    )
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
