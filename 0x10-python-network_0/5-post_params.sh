2263f0d59f3c031dfa7baa8bb3e9234472e72659
Jasmine <jasmineolofin@gmail.com>
2022-09-22 21:51:09 -0700

    Added quote around the value

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script uses curl to perform post method
-curl -sF email=test@gmail subject=I will always be here for PLD $1
+curl -sF email='test%40gmail%2Ecom' subject='I+will+always+be+here+for+PLD' $1
