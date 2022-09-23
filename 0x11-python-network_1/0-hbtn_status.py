b7e2072a980c745d20137b12d8141119ea3b2cc3
Jasmine <jasmineolofin@gmail.com>
2022-09-23 10:27:00 -0700

    Used decode attribute on parsed response object

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with request.urlopen(url) as response:
         body = response.read()
         msg = '\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'\
-            .format(type(body), body, response.msg)
+            .format(type(body), body, body.decode("utf-8"))
 
         print('Body response:')
         print(msg)
