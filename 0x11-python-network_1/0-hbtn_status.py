fac19d7e56175e495f186acb59f84d8dceec1f3f
Jasmine <jasmineolofin@gmail.com>
2022-09-23 10:17:38 -0700

    Added a script to fetch with get request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Script fetches data from https://alx-intranet.hbtn.io/status'''
+
+if __name__ == '__main__':
+    from urllib import request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        body = response.read()
+        msg = '\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'\
+            .format(type(body), body, response.msg)
+
+        print('Body response:')
+        print(msg)
