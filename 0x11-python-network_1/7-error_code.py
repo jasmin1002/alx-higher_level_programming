8a439cf2e7b53cb564e50b05a9fb8766c993b0c9
jasmin1002 <jasmineolofin@gmail.com>
2022-09-23 17:47:45 -0700

    Added a script for requests' status code demo

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    Script demonstrates HTTP status code
+'''
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+    
+    # url
+    url = argv[1]
+    
+    # Make HTTP GET request
+    res = requests.get(url)
+
+    # Retrieve request status code
+    s_code = res.status_code
+
+    if s_code >= 400:
+        print('Error code: {}'.format(s_code))
+    else:
+        print(res.text)
