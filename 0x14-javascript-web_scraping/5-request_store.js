c03483f0d570e47d3df8956ba172f0623f1a8320
Jasmine <jasmineolofin@gmail.com>
2022-10-18 20:58:17 -0700

    feat: Update code with semistandard rules

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,15 +3,15 @@ const request = require('request');
 const fs = require('fs');
 
 if (process.argv.length !== 4) {
-	console.log(`Expect (${2} parameters), got ${process.argv.length - 2}`)
-	process.exit(1)
+  console.log(`Expect (${2} parameters), got ${process.argv.length - 2}`);
+  process.exit(1);
 }
 const parameters = process.argv.splice(2, 2);
 
 request
-	.get(parameters[0])
-	.on('error', err => console.error(err))
-	.on('response', response => {
-		if (response.statusCode === 200) 'Good to go...';
-	})
-	.pipe(fs.createWriteStream(parameters[1]))
+  .get(parameters[0])
+  .on('error', err => console.error(err))
+  .on('response', response => {
+    if (response.statusCode === 200) console.log('Good to go...');
+  })
+  .pipe(fs.createWriteStream(parameters[1]));
