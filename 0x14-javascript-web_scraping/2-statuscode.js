56387bba11def5afad1875f561f2042ad4aa90b9
Jasmine <jasmineolofin@gmail.com>
2022-10-18 17:24:20 -0700

    feat: Modify with http method GET

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@ const request = require('request');
 const url = process.argv[2];
 if (!url) console.log('No url found!');
 else {
-  request(url, (error, response) => {
+  request.get(url, (error, response) => {
     if (error) console.log(error);
     else console.log('code: ', response && response.statusCode);
   });
