804073181e14ffa72f493fb836048ff31bc40be3
Jasmine <jasmineolofin@gmail.com>
2022-10-18 18:24:27 -0700

    fix: Update url variable with let keyword

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 const request = require('request');
 const id = process.argv[2];
 if (id) {
-  url = `https://swapi-api.hbtn.io/api/films/${id}/`;
-  request.get(`${url}`, (error, response, body) => {
+  const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+  request.get(url, (error, response, body) => {
     if (error) console.log(error);
     else if (response.statusCode === 200) {
       console.log(JSON.parse(body).title);
